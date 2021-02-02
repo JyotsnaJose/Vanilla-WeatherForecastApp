5613c7f6814e1d00dfe39fb720dcbc52fd078355
Jyotsna Jose <jyotsnamaria@gmail.com>
2021-02-01 19:08:28 -0600

    Added javascript for API response data display.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 function getCurrentWeather(response) {
-  console.log(response);
+  console.log(response.data);
+  let cityNameElement = document.querySelector("#cityName");
+  cityNameElement.innerHTML = response.data.name;
+  let tempElement = document.querySelector("#temperature");
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  let tempHighElement = document.querySelector("#tempHigh");
+  tempHighElement.innerHTML = Math.round(response.data.main.temp_max);
+  let tempLowElement = document.querySelector("#tempLow");
+  tempLowElement.innerHTML = Math.round(response.data.main.temp_min);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windSpeedElement = document.querySelector("#windSpeed");
+  windSpeedElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  let feelsLikeElement = document.querySelector("#feelsLike");
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
 }
 
 let apiKey = "77284b6440cc462afb48cef654bc731c";
